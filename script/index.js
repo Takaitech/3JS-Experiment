5e1865f0f0d49df3b6c0fc7615e68e04bd1a316d
Taylor Ward <takai.tech2144@gmail.com>
2020-04-24 20:29:47 -0700

    built particle system

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 
 let scene, camera, renderer
+let controls
 let width = window.innerWidth
 let height =  window.innerHeight
 
@@ -7,40 +8,146 @@ function init() {
     //Scene
     scene = new THREE.Scene();
 
+    createCamera();
+    createLights();
+    createMeshes();
+    createRenderer();
+    createParticles();
+
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    renderer.setAnimationLoop(() => {
+        update();
+        render();
+    });
+}
+
+function createCamera() {
+    camera = new THREE.PerspectiveCamera( 75, width/height, .5, 1000 ) 
+    camera.position.z = 5
+
+
+}
+
+function createLights() {
+
     let sceneLight = new THREE.DirectionalLight(0Xffffff, 0.8);
     sceneLight.position.set(0,1,60);
     scene.add(sceneLight)
 
-    //PerspectiveCamera(FOV, Aspect Ratio, Near, Far)
-    camera = new THREE.PerspectiveCamera( 75, width/height, 0.1, 1000 ) 
-
-    //Renderer (the magic)
-    renderer = new THREE.WebGLRenderer(    {antialias: true}
-        );
-    renderer.setClearColor(0xffffff,1)
-    renderer.setSize( width, height );
-    document.body.appendChild(renderer.domElement);
-
-    camera.position.z = 5
 }
 
 let cube
 let cubeGeometry, cubeMaterial
 
-function createcube() {
-    cubeGeometry = new THREE.BoxGeometry();
-    cubeMaterial = new THREE.MeshBasicMaterial({color:0x00ff00});
+let line
+
+function createMeshes() {
+    cubeGeometry = new THREE.BoxBufferGeometry();
+    cubeMaterial = new THREE.MeshLambertMaterial();
     
     cube = new THREE.Mesh( cubeGeometry, cubeMaterial);
-    cube.position.z += -.2;
-    // scene.add(cube);
+    cube.position.z += 3;
+    scene.add(cube);
+
+    //Buffer cubeGeometry
+
+    var MAX_POINTS = 1000;
+
+    // cubeGeometry
+    var Geometry = new THREE.BufferGeometry();
+
+    // attributes
+    var positions = new Float32Array( MAX_POINTS * 3 ); // 3 vertices per point
+    Geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+
+    // draw range
+    var drawCount = 1; // draw the first 2 points, only
+    Geometry.setDrawRange( 0, drawCount );
+
+    // cubeMaterial
+    var Material = new THREE.LineBasicMaterial( { color: 0xffffff} );
+
+    // line
+    line = new THREE.Line( Geometry,  Material );
+    scene.add( line );
+
+    var positions = line.geometry.attributes.position.array;
+
+    var x, y, z, index;
+    x = y = z = index = 0;
+
+    for ( var i = 0, l = MAX_POINTS; i < l; i ++ ) {
+
+        positions[ index ++ ] = x;
+        positions[ index ++ ] = y;
+        positions[ index ++ ] = z;
+
+        x = Math.random() * 500 - 250,
+        y = Math.random() * 500 - 250,
+        z = Math.random() * 500 - 250
+
+    }
+
+    line.geometry.attributes.position.needsUpdate = true
+
+}
+
+let particleSystem
+
+function createParticles() {
+    let particleCount = 1800;
+    let particles = new THREE.BufferGeometry();
+    let pMaterial = new THREE.PointsMaterial({size: 3, color: 0xffffff})
+
+    let vertices = []
+    for( let p = 0; p < particleCount; p ++) {
+        let pX = Math.random() * 500 - 250,
+            pY = Math.random() * 500 - 250,
+            pZ = Math.random() * 500 - 250,
+            particle = (pX,pY,pZ)
+
+        vertices.push(particle)
+
+    }
+
+    particles.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+    particleSystem = new THREE.Points(particles,pMaterial);
+    scene.add(particleSystem)
+}
+
+function createRenderer() {
+    renderer = new THREE.WebGLRenderer(    {antialias: true}
+        );
+    renderer.setPixelRatio( window.devicePixelRatio )
+    renderer.setClearColor(0x000000,1)
+    renderer.setSize( width, height );
+    document.body.appendChild(renderer.domElement);
+
 }
 
 
-init();
-createcube();
-// particleSetup();
 
+
+
+function update() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    // portalParticles.forEach(p => {
+    //     p.rotation.z += 0.01;
+    //     p.position.z = p.position.z - Math.random() / 100;
+    // });
+
+    // camera.position.z += 0.08;
+    line.geometry.setDrawRange( 0, line.geometry.drawRange.count + 0.05);
+}
+
+function render() {
+    controls.update();
+
+    renderer.render(scene, camera)
+}
 
 window.addEventListener( 'resize', onWindowResize, false );
 
@@ -53,86 +160,4 @@ function onWindowResize(){
 
 }
 
-
-//Buffer cubeGeometry
-
-var MAX_POINTS = 1000;
-
-// cubeGeometry
-var Geometry = new THREE.BufferGeometry();
-
-// attributes
-var positions = new Float32Array( MAX_POINTS * 3 ); // 3 vertices per point
-Geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-
-// draw range
-var drawCount = 1; // draw the first 2 points, only
-Geometry.setDrawRange( 0, drawCount );
-
-// cubeMaterial
-var Material = new THREE.LineBasicMaterial( { color: 0xffffff } );
-
-// line
-var line = new THREE.Line( Geometry,  Material );
-scene.add( line );
-
-var positions = line.geometry.attributes.position.array;
-
-var x, y, z, index;
-x = y = z = index = 0;
-
-for ( var i = 0, l = MAX_POINTS; i < l; i ++ ) {
-
-    positions[ index ++ ] = x;
-    positions[ index ++ ] = y;
-    positions[ index ++ ] = z;
-
-    x += ( Math.random() - 0.5 ) * 30;
-    y += ( Math.random() - 0.5 ) * 30;
-    z += ( Math.random() - 0.5 ) * 30;
-
-}
-
-line.geometry.attributes.position.needsUpdate = true
-
-
-// let portalParticles = []
-
-// function particleSetup() {
-//     let loader = new THREE.TextureLoader();
-//     loader.load("script/texture.png", function(texture) {
-//         let geo = new THREE.PlaneBufferGeometry(20,20)
-//         let mat = new THREE.MeshStandardMaterial({
-//             map:texture,
-//             transparent: true
-//         });
-
-//         for(let p = 10; p > 1; p--) {
-//             let particle = new THREE.Mesh(geo, mat);
-//             particle.position.set( Math.random(), Math.random() , -10);
-//             particle.rotation.z = Math.random() *360;
-//             portalParticles.push(particle);
-//             scene.add(particle)
-//         }
-        
-//     })
-// }
-
-
-
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    // portalParticles.forEach(p => {
-    //     p.rotation.z += 0.01;
-    //     p.position.z = p.position.z - Math.random() / 100;
-    // });
-
-    // camera.position.z += 0.08;
-    line.geometry.setDrawRange( 0, line.geometry.drawRange.count + 0.05);
-
-    renderer.render(scene, camera);
-
-}
-animate();
+init();
